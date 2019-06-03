a7de9a6670e7c14acbb7bc63631c35522f80cd0b
ANGEL DE JESUS GONZALEZ PEREZ <173237@ids.upchiapas.edu.mx>
2019-06-03 12:10:47 -0500

    as

--- Alumno/urls.py
+++ Alumno/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, re_path
 from django.conf.urls import include
 from django.contrib.auth.models import User
-# from rest_framework import routers, serializers, viewsets
-# importa todas las vistas exclusivas de la aplicacion 
 from . import views 
 
 urlpatterns = [
